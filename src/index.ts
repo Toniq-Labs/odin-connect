73df16da14a4bb65289f7b0e873c09af15d3d691
Crisdell James Ines <crisdelli@codev.com>
2025-09-24 10:33:10 +0800

    Export and update token model to full

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { Connect as OdinConnect } from "./services/connect";
 export type { User as OdinUser } from "./models/user";
 export type { Balance as OdinBalance } from "./models/balance";
+export type { Token as OdinToken } from "./models/token";
+export type { BaseToken as OdinBaseToken } from "./models/token";
--- src/models/token.ts
+++ src/models/token.ts
@@ -1,4 +1,4 @@
-export interface Token {
+export interface BaseToken {
   id: string;
   name: string;
   ticker: string;
@@ -6,4 +6,48 @@ export interface Token {
   decimals: number;
   withdrawals: boolean;
   deposits: boolean;
+}
+
+export interface Token extends BaseToken {
+  description: string | null;
+  creator: string;
+  created_time: Date;
+  marketcap: bigint;
+  volume: bigint;
+  volume_24: number;
+  holder_count: number;
+  power_holder_count: number;
+  sell_count: number;
+  buy_count: number;
+  comment_count: number;
+  featured: boolean;
+  bonded: boolean;
+  external: boolean;
+  trading: boolean;
+  icrc_ledger: string | null;
+  sold: bigint;
+  total_supply: bigint;
+  threshold: bigint;
+  swap_volume: bigint | null;
+  swap_volume_24: bigint | null;
+  holding_value: number | null;
+  price: number;
+  twitter: string | null;
+  twitter_verified: boolean | null;
+  website: string | null;
+  telegram: string | null;
+  btc_liquidity: bigint;
+  token_liquidity: bigint;
+  liquidity_threshold: bigint;
+  last_comment_time: string | null;
+  last_action_time: Date | null;
+  user_lp_tokens: bigint;
+  user_btc_liquidity: bigint;
+  user_token_liquidity: bigint;
+  verified: boolean;
+  progress: number | null;
+  price_5m: number | null;
+  price_1h: number | null;
+  price_6h: number | null;
+  price_1d: number | null;
 }
