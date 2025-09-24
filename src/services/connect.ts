1f3d7a1bb553135be8734f24f8798d430a6a95d0
Crisdell James Ines <crisdelli@codev.com>
2025-09-24 09:49:22 +0800

    add deployment preview env

--- src/services/connect.ts
+++ src/services/connect.ts
@@ -5,6 +5,7 @@ const ORIGINS = {
   local: "http://localhost:5173",
   prod: "https://odin.fun",
   dev: "https://dev.odin.fun",
+  _deployment_preview: "https://deploy-preview-1047--dev-odin-toniq.netlify.app"
 };
 
 interface AppInitOptions {
