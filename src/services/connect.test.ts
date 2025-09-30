ea3a8b6844f4ef5f802ddf628cebeaa90e71d347
Crisdell James Ines <crisdelli@codev.com>
2025-09-30 13:41:46 +0800

    add test cases to connect.ts

--- src/services/connect.test.ts
+++ src/services/connect.test.ts
@@ -1,7 +1,15 @@
 import { describe, it, expect, beforeAll, vi } from "vitest";
 import { Connect } from "./connect";
+import { Token } from "../models/token";
 
-describe("sample test", () => {
+const mockTokens = [
+  {
+    id: "2jjj",
+    ticker: "TEST",
+  },
+] as Token[];
+
+describe("Connect", () => {
   let connect: Connect;
 
   beforeAll(() => {
@@ -28,5 +36,112 @@ describe("sample test", () => {
     expect(url.pathname).toBe("/authorize/test");
     expect(url.searchParams.get("app_name")).toBe("TestApp");
     expect(url.searchParams.get("referrer")).toBeDefined();
+  });
+
+  it("should fetch all tokens", async () => {
+    connect["_api"].getTokens = vi.fn().mockResolvedValue({
+      data: mockTokens,
+      count: mockTokens.length,
+      page: 1,
+      limit: 10,
+    });
+    const tokens = await connect.getTokens({
+      pagination: { page: 1, limit: 10 },
+      sort: { field: "price", direction: "desc" },
+    });
+    expect(connect["_api"].getTokens).toHaveBeenCalledWith(
+      {
+        page: 1,
+        limit: 10,
+      },
+      { field: "price", direction: "desc" }
+    );
+    expect(tokens.data).toEqual(mockTokens);
+  });
+
+  it("should fetch all balances", async () => {
+    connect["_api"].getBalances = vi.fn().mockResolvedValue([]);
+    const result = await connect.getBalances({
+      principal: "test-principal",
+      pagination: { page: 1, limit: 10 },
+    });
+    expect(connect["_api"].getBalances).toHaveBeenCalledWith("test-principal", {
+      page: 1,
+      limit: 10,
+    });
+    expect(result).toEqual([]);
+  });
+
+  it("should open the connect window", () => {
+    const openSpy = vi.spyOn(window, "open").mockImplementation(() => null);
+
+    // test both requires_api true and false
+    connect.connect({ requires_api: false });
+    connect.connect({ requires_api: true });
+    connect.connect({
+      open: {
+        target: "_blank",
+        settings: "height=600,width=400,left=100,top=100",
+      },
+    });
+    expect(openSpy).toHaveBeenCalledTimes(3);
+    for (let i = 0; i < openSpy.mock.calls.length; i++) {
+      const url = openSpy.mock.calls[i][0] as URL;
+
+      expect(url).toBeInstanceOf(URL);
+      expect(url?.toString()).toContain(
+        "http://localhost:5173/authorize/connect?"
+      );
+      const params = new URLSearchParams(url?.search);
+      expect(params.get("app_name")).toBe("TestApp");
+      expect(params.get("referrer")).toBeDefined();
+      if (i === 0) {
+        expect(params.get("requires_api")).toBe("0");
+      } else if (i === 1) {
+        expect(params.get("requires_api")).toBe("1");
+      } else if (i === 2) {
+        expect(params.get("requires_api")).toBe("0");
+        expect(openSpy.mock.calls[i][1]).toBe("_blank");
+        expect(openSpy.mock.calls[i][2]).toBe(
+          "height=600,width=400,left=100,top=100"
+        );
+      }
+    }
+  });
+
+  it("should open the buy authorization window", () => {
+    const openSpy = vi.spyOn(window, "open").mockImplementation(() => null);
+    connect.buy({
+      token: "2jjj",
+      btcAmount: 20001n,
+      principal: "test-principal",
+    });
+    expect(openSpy).toHaveBeenCalled();
+    const url = openSpy.mock.calls[0][0] as URL;
+    expect(url).toBeInstanceOf(URL);
+    expect(url?.toString()).toContain("http://localhost:5173/authorize/buy?");
+    const params = new URLSearchParams(url?.search);
+    expect(params.get("app_name")).toBe("TestApp");
+    expect(params.get("token")).toBe("2jjj");
+    expect(params.get("amount")).toBe("20001");
+    expect(params.get("principal")).toBe("test-principal");
+  });
+
+  it("should open the sell authorization window", () => {
+    const openSpy = vi.spyOn(window, "open").mockImplementation(() => null);
+    connect.sell({
+      token: "2jjj",
+      tokenAmount: 32000n,
+      principal: "test-principal",
+    });
+    expect(openSpy).toHaveBeenCalled();
+    const url = openSpy.mock.calls[0][0] as URL;
+    expect(url).toBeInstanceOf(URL);
+    expect(url?.toString()).toContain("http://localhost:5173/authorize/sell?");
+    const params = new URLSearchParams(url?.search);
+    expect(params.get("app_name")).toBe("TestApp");
+    expect(params.get("token")).toBe("2jjj");
+    expect(params.get("amount")).toBe("32000");
+    expect(params.get("principal")).toBe("test-principal");
   });
 });
