b40a1ec6c152a2cea2870301e2214452414ed815
Crisdell James Ines <crisdelli@codev.com>
2025-10-23 13:25:32 +0800

    update example canister

--- demo/src/features/Canister.tsx
+++ demo/src/features/Canister.tsx
@@ -2,12 +2,15 @@ import { useEffect, useState } from "react";
 import { useOdinContext } from "../OdinContext";
 import { Actor, HttpAgent } from "@dfinity/agent";
 import { DelegationIdentity } from "@dfinity/identity";
-import { idlFactory, type _SERVICE } from "../canister/odin-dev";
+import { idlFactory, type _SERVICE } from "../canister/test";
 import { DEMO_CANISTER_ID, DEMO_IC_HOST } from "../constants";
+import JSONBig from "@apimatic/json-bigint"
 
 export function Canister() {
     const { user, delegationChain, sessionKey } = useOdinContext();
     const [agent, setAgent] = useState<HttpAgent | null>(null);
+    const [pending, setPending] = useState(false);
+    const [result, setResult] = useState<unknown | null>(null);
 
     useEffect(() => {
         if (delegationChain && sessionKey) {
@@ -30,6 +33,7 @@ export function Canister() {
 
     const handleCanisterCall = async () => {
         try {
+            setPending(true);
             if (!agent) {
                 throw new Error("Agent is not initialized");
             }
@@ -39,10 +43,13 @@ export function Canister() {
             });
             console.log("Calling canister...");
             console.log("Agent:", agent);
-            const result = await canisterApi.getToken("2jj2");
+            const result = await canisterApi.icrc1_name();
             console.log("Token info:", result);
+            setResult((result));
         } catch (error) {
             console.error("Error occurred:", error);
+        } finally {
+            setPending(false);
         }
     };
 
@@ -54,11 +61,9 @@ export function Canister() {
     }
     return (
         <div>
-            <button onClick={handleCanisterCall}>Get Token (2jj2)</button>
+            <button onClick={handleCanisterCall}>Run icrc1_name()</button>
             <br />
-            <small>
-                Look at the console for results.
-            </small>
+            <pre style={{ height: 350, overflow: 'auto'}}>{pending ? "Loading..." : JSONBig.stringify(result, null, 2)}</pre>
         </div>
     );
 }
