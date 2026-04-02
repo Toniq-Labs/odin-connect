44005b7b4db9023cf0c961cb1084726059e8e116
Crisdell James Ines <crisdelli@codev.com>
2026-04-02 11:20:12 +0800

    feat[86agj5cad]:  add preview origin

--- src/models/environment.ts
+++ src/models/environment.ts
@@ -2,6 +2,7 @@ export const ORIGINS = {
   local: "http://localhost:5173",
   prod: "https://odin.fun",
   dev: "https://dev.odin.fun",
+  _preview: "https://deploy-preview-1368--dev-odin-toniq.netlify.app",
 };
 
 export type Environment = keyof typeof ORIGINS;
